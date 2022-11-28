5c45ecf9f92b576f03746812b5019846fd2c6553
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-11-29 00:26:29 +0530

    Class 6

---
+++ class6-ifElifElse-CipherSchools.py
@@ -0,0 +1,20 @@
+a = True
+if a:
+    print("The value is True")
+print("end")
+
+a = False
+if a:
+    print("The value is True")
+else:
+    print("The value is False")
+
+a = 5
+if a == 1:
+    print("The value is 1")
+elif a == 5:
+    print("The value is 5")
+else:
+    print("The value is neither 5 nor 1")
+
+# Use K-Maps to find simpler expressions for complex scenerios
