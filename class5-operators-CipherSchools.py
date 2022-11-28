1e7ea75a7c805e59fb90c904d09e097555fd269e
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-11-28 23:22:57 +0530

    Class 5

---
+++ class5-operators-CipherSchools.py
@@ -0,0 +1,37 @@
+# Arithmetic
+print(5+5)
+print(10-5)
+# Python 3 introduced a new datatype - 'number' hence 4/2 results in a float even though operands are integers
+print(4/2)
+print(4//2)
+print(4**2)
+print("Yash" + "Gupta")
+print("Yash"*5)
+print("Yash" " " " Gupta")
+
+# Comparision
+print(5 < 2)
+print(6 > 7)
+print(8 == 8)
+print(8 != 8)
+print(7 >= 7)
+print(8 <= 11)
+print("ab" < "z")
+print("a" < "A")
+
+# Remember
+# true or a = true          -> a or b = a (if a is truthy)
+# false or a = a            -> a or b = b (if a is falsy)
+# true and a = a            -> a and b = b (if a is truthy)
+# false and a = false       -> a and b = a (if a is falsy)
+
+# Truthy = True, positive-numbers, non-empty strings, non-empty array
+# Falsy = False, 0, "", []
+
+print("12134" or False)
+print(0 and 1)
+print(1 and 5)
+print("" and True)
+print(0 and "")
+print([1,2,3] or True)
+
