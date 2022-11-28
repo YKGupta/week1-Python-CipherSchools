f9563aaa28cda7c20dc9473bf75bcbaf937dd2e6
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-11-28 22:18:29 +0530

    Class 4

---
+++ class4-inputTypeConversion.py
@@ -0,0 +1,9 @@
+a = input()
+print(a)
+print(type(a))
+# Python does not have primitives 
+# It only has objects ie all things belong to the object class
+a = 15
+print(str(a))
+print(int('1234'))
+print(float('1.5'))
