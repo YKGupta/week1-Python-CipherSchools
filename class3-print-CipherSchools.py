7c4dad60032cad8f8d92c660503add2ad05365bb
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-11-28 21:08:50 +0530

    Class 3

---
+++ class3-print-CipherSchools.py
@@ -0,0 +1,12 @@
+print('Yash Kumar Gupta')
+
+print('hi', 4, 4.5, 16 + 9j, 'bye')
+
+print('hello')
+print('byeee!')
+
+print('hi', 'bye', 45, 55, 5.6, 17 + 0j, sep = "--")
+
+print('hii', end = "{")
+print('byeee', end  = "(")
+print('skyyy')
